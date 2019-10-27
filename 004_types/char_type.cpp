e2fd5ebb83bf649a437dfe834a4985d27651ff63
neo <neo@example.ru>
2019-10-27 11:34:30 +0300

    const

--- 004_types/char_type.cpp
+++ 004_types/char_type.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
 int main(){
+    unsigned char u_ch{}; // явное указание
+    signed char s_ch{};
+    
+    wchar_t w_ch{L'A'}; // расширенный набор типов
+    std::wcout << w_ch << std::endl;
+    
+    char16_t ch16{u'a'};
+    char32_t ch32{U'B'};
+  
     char ch{};
     std::cout << "Введите символ: ";
     std::cin >> ch;
-
+        
     std::cout << "Символ : " << ch << std::endl;
     std::cout << "ASCII : " << int(ch) << std::endl;
+    
+    std::cout << "cout.put : ";
+    std::cout.put(ch);
+    std::cout << std::endl;
 }
---
+++ 004_types/const_var.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    const int MAX_SIZE = 32;
+    std::cout << MAX_SIZE << std::endl;
+}
